1588e715cd8251bdd458ffc01366c4403d9d7092
GabeLove <gabriel_ballesteros98@hotmail.com>
2022-10-02 19:05:49 -0500

    Interfaz de registro hecho y Conexion con Firebase

--- app/src/main/java/com/example/primedentapp/InicioSesion.kt
+++ app/src/main/java/com/example/primedentapp/InicioSesion.kt
@@ -1,11 +1,46 @@
 package com.example.primedentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.primedentapp.databinding.ActivityInicioSesionBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class InicioSesion : AppCompatActivity() {
+
+    private lateinit var binding:ActivityInicioSesionBinding
+    private lateinit var  firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inicio_sesion)
+        binding = ActivityInicioSesionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this,RegistroSesion::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            val email = binding.textInputEditText.text.toString()
+            val pass = binding.passInputEditText.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+            }else{
+                Toast.makeText(this,"Ingrese datos porfavor", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/primedentapp/RegistroSesion.kt
+++ app/src/main/java/com/example/primedentapp/RegistroSesion.kt
@@ -1,11 +1,53 @@
 package com.example.primedentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.primedentapp.databinding.ActivityRegistroSesionBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistroSesion : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegistroSesionBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_sesion)
+
+        binding = ActivityRegistroSesionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, InicioSesion::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passInputEditText.text.toString()
+            val confirmPass = binding.passConfInputEditText.text.toString()
+
+
+            if(email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if(pass ==confirmPass){
+                    firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            val intent = Intent(this, InicioSesion::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }else{
+                    Toast.makeText(this,"La constraseña no coincide", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this,"Ingrese datos porfavor", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
